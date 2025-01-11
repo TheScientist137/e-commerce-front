440ebd96229d215bb019c1d2a02deab8620aa1e2
The-Scientist <thescientist137@outlook.es>
2025-01-11 11:11:15 +0100

    capture signup data (the old way) and send it to backend succesfully -- created new users (tests)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
     <main>
       <header>Astro Shop</header>
       <Signup />
+      <p>Already have an account?</p>
+      <a href="http://">Login</a>
     </main>
   )
 }
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,35 +1,35 @@
-import { z } from "zod"
-
-const user = z.object({
- username: z.string(),
- email: z.string(),
- password: z.string()
-})
-const partialUser = user.partial({ username: true })
+import { useState } from "react";
 
 export default function Signup() {
- const formAction = (formData: FormData) => {
-  const formValues = Object.fromEntries(formData)
-  const results = partialUser.safeParse(formValues)
+ const [formData, setFormData] = useState({ name: '', email: '', password: '' });
 
-  if (results.success) {
-   console.log(results.data)
-  } else {
-   console.log('error')
-  }
+ const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setFormData((prevData) => {
+  return { ...prevData, [event.target.name]: event.target.value }
+ });
 
+ const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+
+  const response = await fetch('http://localhost:3000/api/auth/signup', {
+   method: 'POST',
+   headers: { 'Content-Type': 'application/json' },
+   body: JSON.stringify(formData)
+  });
+
+  const result = await response.json();
+  console.log(result);
  }
 
  return (
-  <form action={formAction}>
+  <form onSubmit={handleSubmit}>
    <label htmlFor="name">Name</label>
-   <input type="text" name="username" />
+   <input type="text" name="name" value={formData.name} onChange={handleChange} />
 
    <label htmlFor="email">Email</label>
-   <input type="text" name="email" />
+   <input type="email" name="email" value={formData.email} onChange={handleChange} />
 
    <label htmlFor="password">Password</label>
-   <input type="text" name="password" />
+   <input type="password" name="password" value={formData.password} onChange={handleChange} />
 
    <button>Signup</button>
   </form>
