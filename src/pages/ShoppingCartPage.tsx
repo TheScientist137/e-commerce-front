c3a077f704cb8e44320b799067b8d589f61c6916
The-Scientist <thescientist137@outlook.es>
2025-03-05 13:27:24 +0100

    condicional shopping cart view

--- src/pages/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage.tsx
@@ -10,18 +10,26 @@ export default function ShoppingCartPage() {
 
  return (
   <section>
-   <div>
-    {cartItems.map((item) => (
-     <div key={item.name}>
-      <p>{item.name}</p>
-      <p>{item.brand}</p>
-      <button>-</button>
-      <button>+</button>
-      <p>{item.price} $</p>
-     </div>
-    ))}
-   </div>
+   {
+    cartItems ? (
+     <div>
+      <button>Proceed to checkout</button>
+      <div>
+       {cartItems.map((item) => (
+        <div key={item.name}>
+         <p>{item.name}</p>
+         <p>{item.brand}</p>
+         <button>-</button>
+         <button>+</button>
+         <button>DELETE</button>
+         <p>{item.price} $</p>
+        </div>
+       ))}
+      </div>
 
+     </div>
+    ) : <p>There are no products in the shopping cart yet</p>
+   }
    <Link to='/'>Back to shop</Link>
   </section>
  )
