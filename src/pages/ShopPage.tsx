69f620c57926b1e648a2b5dd96a89f6c2c23df21
The-Scientist <thescientist137@outlook.es>
2025-03-12 12:11:04 +0100

    change Link for NavLink

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 export default function ShopPage() {
   return (
     <section>
-      <Link to='/telescopes' >telescopes</Link>
-      <Link to='/mounts' >mounts</Link>
+      <div>
+      <NavLink to='/telescopes' >telescopes</NavLink>
+      </div>
+      <NavLink to='/mounts' >mounts</NavLink>
     </section>
   )
 }
